5e90618ebde42317f5e39577a66b22c65fb30215
Earl Lewis <earl.lewis.1@gmail.com>
2023-11-28 16:23:00 -0700

    added styling and fixed show/hide complete button

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,13 +1,41 @@
 import { Template } from 'meteor/templating';
 import { TasksCollection } from '../api/TasksCollection';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import './App.html'; 
 import './Tasks.js';
+
+const HIDE_COMPLETED_STRING = "hideCompleted";
+
+Template.mainContainer.onCreated(function mainContainerOnCreated(){
+    this.state = new ReactiveDict();
+})
+
+Template.mainContainer.events({
+    'click #hide-completed-button'(event, instance) {
+        const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+        instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
+    }
+})
+
+
 
 
 Template.mainContainer.helpers({
     tasks() {
-        return TasksCollection.find({}, { sort: {createdAt: -1} });
-    }
+        const instance = Template.instance();
+        const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+
+        const hideCompletedFilter = { isChecked: { $ne: true}};
+        return TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, 
+            { sort: {createdAt: -1} }).fetch();
+    },
+    hideCompleted() {
+        return Template.instance().state.get(HIDE_COMPLETED_STRING);
+    },
+    incompleteCount() {
+        const incompleteTasksCount = TasksCollection.find({isChecked: {$ne: true}}).count();
+        return incompleteTasksCount ? `(${incompleteTasksCount})` : '';
+    },
 });
 
 Template.form.events({
