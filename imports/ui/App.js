c145c32eb86a67ad276ea7b73e643601e58e8cdf
Earl Lewis <earl.lewis.1@gmail.com>
2023-11-28 14:48:15 -0700

    added connection to database and form UI

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,28 @@
 import { Template } from 'meteor/templating';
-
+import { TasksCollection } from '../api/TasksCollection';
 import './App.html'; 
+import './Tasks.js';
+
 
 Template.mainContainer.helpers({
-    tasks: [
-        {text: 'This is link 1'},
-        {text: 'This is link 2'},
-        {text: 'This is link 3'},
-    ]
+    tasks() {
+        return TasksCollection.find({}, { sort: {createdAt: -1} });
+    }
 });
+
+Template.form.events({
+    "submit .task-form"(event) {
+        // prevent default browser form submit
+        event.preventDefault();
+
+        const target = event.target;
+        const text = target.text.value;
+
+        TasksCollection.insert({
+            text,
+            createdAt: new Date(),
+        });
+
+        target.text.value = "";
+    }
+})
