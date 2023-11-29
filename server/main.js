728c7bd0cda61cb1d65eecec374cf21897275545
Earl Lewis <earl.lewis.1@gmail.com>
2023-11-29 07:01:49 -0700

    added publish/subscribe

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { TasksCollection } from '/imports/db/TasksCollection';
 import { Accounts } from 'meteor/accounts-base';
 import '/imports/api/TasksMethods';
+import '/imports/api/TasksPublications';
 
  
 const insertTask = (taskText, user)  => TasksCollection.insert(
